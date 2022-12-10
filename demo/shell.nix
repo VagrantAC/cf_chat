73c3b81aafb72b31af99d8326dcd96a1f0d5a381
VagrantAC <19903487846@163.com>
2022-12-10 23:42:26 +0800

    xxx

--- demo/shell.nix
+++ demo/shell.nix
@@ -2,12 +2,10 @@
 
 pkgs.mkShell {
   nativeBuildInputs = [
-    pkgs.neovim
 	  pkgs.nodePackages.yarn
   	pkgs.nodejs-16_x
     pkgs.go_1_18
     pkgs.golangci-lint
-    pkgs.neovim
     pkgs.protobuf3_8
     pkgs.protoc-gen-go
     pkgs.protoc-gen-go-grpc
